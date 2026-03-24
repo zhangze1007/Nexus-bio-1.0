646b019010fead5d947f81c640b2759a0291fdc4
Zhang Ze <fuchanze@gmail.com>
2026-03-24 16:40:34 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.'),
+      "@": path.resolve(__dirname, "."),
     },
   },
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
-        privacy: path.resolve(__dirname, 'privacy.html'),
-        terms: path.resolve(__dirname, 'terms.html'),
+        main: path.resolve(__dirname, "index.html"),
+        privacy: path.resolve(__dirname, "privacy.html"),
+        terms: path.resolve(__dirname, "terms.html"),
       },
     },
   },
-})
+});
